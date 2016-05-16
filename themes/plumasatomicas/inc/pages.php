dc5efa433243dc22c24c0c7a062a7bf3607e7ac6
Pablo Covarrubias <cova@covapro.local>
2016-05-16 15:03:06 -0500

    merge

--- themes/plumasatomicas/inc/pages.php
+++ themes/plumasatomicas/inc/pages.php
@@ -8,16 +8,16 @@
 
 
 		// CONTACTO
-		/*if( ! get_page_by_path('faq') ){
+		if( ! get_page_by_path('opinologo') ){
 			$page = array(
 				'post_author' => 1,
 				'post_status' => 'publish',
-				'post_title'  => 'Frequently Asked Questions',
-				'post_name'   => 'faq',
+				'post_title'  => 'Perfil opinólogo',
+				'post_name'   => 'opinologo',
 				'post_type'   => 'page'
 			);
 			wp_insert_post( $page, true );
-		}*/
+		}
 
 
 	});
