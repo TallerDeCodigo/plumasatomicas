a421e86a4420c6a9ac9f88a7363592a51c1f2bd8
Pablo Covarrubias <cova@covapro.local>
2016-05-16 15:01:52 -0500

    fact checker

--- themes/plumasatomicas/inc/metaboxes.php
+++ themes/plumasatomicas/inc/metaboxes.php
@@ -8,6 +8,7 @@
 	add_action('add_meta_boxes', function(){
 
 		add_meta_box( 'columna_info_general', 'Información general de la columna', 'show_columna_info_general', 'what-the-fact', 'side', 'high' );
+		add_meta_box( 'fact_checker', 'Fact checker', 'show_fact_checker', 'what-the-fact', 'advanced', 'high' );
 
 	});
 
@@ -37,6 +38,38 @@
 		echo "<input type='text' class='widefat' id='source_url_columna' name='source_url_columna' value='$source_url_columna'/>";
 	}
 
+	function show_fact_checker($post){
+		$argumento_uno = get_post_meta($post->ID, 'argumento_uno', true);
+		$calif_argumento_uno = get_post_meta($post->ID, 'calif_argumento_uno', true);
+		$argumento_dos = get_post_meta($post->ID, 'argumento_dos', true);
+		$calif_argumento_dos = get_post_meta($post->ID, 'calif_argumento_dos', true);
+		wp_nonce_field(__FILE__, 'fact_checker_nonce');
+
+		echo "<label>Argumento 1:</label>";
+		echo "<textarea class='widefat' rows='4' cols='50' id='argumento_uno' name='argumento_uno' value='$argumento_uno'>".$argumento_uno."</textarea>";
+		echo "<br /><br />";
+		echo "<label>Calificación:  </label>";
+		echo "<select id='calif_argumento_uno' name='calif_argumento_uno'>";
+		echo "<option value='verdadero'".selected( $calif_argumento_uno, 'verdadero' ).">verdadero</option>";
+		echo "<option value='mayoritariamente_verdadero' ".selected( $calif_argumento_uno, 'mayoritariamente_verdadero' ).">Mayoritariamente verdadero</option>";
+		echo "<option value='verdades_descontextualizadas' ".selected( $calif_argumento_uno, 'verdades_descontextualizadas' ).">Verdades descontextualizadas</option>";
+		echo "<option value='falso'>Falso</option>";
+		echo "<option value='escandalosamente_falso' ".selected( $calif_argumento_uno, 'escandalosamente_falso' ).">Escandalosamente falso</option>";
+		echo "</select>";
+		echo "<br /><br />";
+		echo "<label>Argumento 2</label>";
+		echo "<textarea class='widefat' rows='4' cols='50' id='argumento_dos' name='argumento_dos' value='$argumento_dos'>".$argumento_dos."</textarea>";
+		echo "<br /><br />";
+		echo "<label>Calificación:  </label>";
+		echo "<select id='calif_argumento_dos' name='calif_argumento_dos'>";
+		echo "<option value='verdadero'".selected( $calif_argumento_dos, 'verdadero' ).">verdadero</option>";
+		echo "<option value='mayoritariamente_verdadero' ".selected( $calif_argumento_dos, 'mayoritariamente_verdadero' ).">Mayoritariamente verdadero</option>";
+		echo "<option value='verdades_descontextualizadas' ".selected( $calif_argumento_dos, 'verdades_descontextualizadas' ).">Verdades descontextualizadas</option>";
+		echo "<option value='falso'>Falso</option>";
+		echo "<option value='escandalosamente_falso' ".selected( $calif_argumento_dos, 'escandalosamente_falso' ).">Escandalosamente falso</option>";
+		echo "</select>";
+	}
+
 
 
 // SAVE METABOXES DATA ///////////////////////////////////////////////////////////////
@@ -63,6 +96,13 @@
 			update_post_meta($post_id, 'nombre_columna', $_POST['nombre_columna']);
 			update_post_meta($post_id, 'source_columna', $_POST['source_columna']);
 			update_post_meta($post_id, 'source_url_columna', $_POST['source_url_columna']);
+		}
+
+		if ( isset($_POST['argumento_uno']) and check_admin_referer(__FILE__, 'fact_checker_nonce') ){
+			update_post_meta($post_id, 'argumento_uno', $_POST['argumento_uno']);
+			update_post_meta($post_id, 'argumento_dos', $_POST['argumento_dos']);
+			update_post_meta($post_id, 'calif_argumento_uno', $_POST['calif_argumento_uno']);
+			update_post_meta($post_id, 'calif_argumento_dos', $_POST['calif_argumento_dos']);
 		}
 
 
--- themes/plumasatomicas/single-what-the-fact.php
+++ themes/plumasatomicas/single-what-the-fact.php
@@ -26,11 +26,8 @@
 			<blockquote class="fact">Proin a felis ac nisi facilisis rhoncus. Donec ac elit et neque luctus hendrerit non non sem. Sed vel nisi urna”<br><span>LOREM IPSUM DOLOR SIT AMET</span></blockquote>
 			<div class="separador"><span>ITERACIÓN</span></div>
 			<p>Aliquam erat volutpat. Morbi in leo tempus, pellentesque ligula at, porta sem. Maecenas dignissim, tellus a malesuada lacinia, nunc tortor accumsan dui, et rhoncus ipsum ante fermentum est. Donec sagittis facilisis ipsum, in consectetur lorem egestas vitae.</p>
-			<code>#HASHTAG</code> <code>#HASHTAG</code> <code>#HASHTAG</code>
-			<div class="separador"></div>
-			<p>Aliquam erat volutpat. Morbi in leo tempus, pellentesque ligula at, porta sem. Maecenas dignissim, tellus a malesuada lacinia, nunc tortor accumsan dui, et rhoncus ipsum ante fermentum est. Donec sagittis facilisis ipsum, in consectetur lorem egestas vitae.</p>
-			<div class="imagen-post"><img src="<?php echo THEMEPATH; ?>images/1.png"><span>FOTO: LOREM IPSUM DOLOR SIT AMET</span></div>
-			<p>Donec velit tellus, varius in viverra eu, pharetra ut lorem. Duis fringilla accumsan ultrices. Maecenas in sapien interdum, malesuada ex non, aliquet nunc. Proin nec est venenatis, aliquam orci rutrum, lobortis nibh. Ut tincidunt id nisi eget fermentum. Nam id tortor nec odio malesuada volutpat. Vestibulum porta id massa vitae convallis. Donec id euismod augue, nec auctor nunc. Suspendisse sed est vel nibh luctus rutrum. Integer erat tortor, pretium eu nibh sit amet, bibendum tincidunt nisl. Aenean quis dictum orci. Aliquam et facilisis dolor. Fusce malesuada elit a purus porta, ut imperdiet tellus blandit. Morbi id dapibus quam. Ut aliquet nulla et eros eleifend venenatis.</p>
+			
+			
 			<nav>
 				<a href="#"><img src="<?php echo THEMEPATH; ?>images/social/fb.png"></a>
 				<a href="#"><img src="<?php echo THEMEPATH; ?>images/social/tw.png"></a>
@@ -39,24 +36,10 @@
 				<a href="#"><img src="<?php echo THEMEPATH; ?>images/social/wp.png"></a>
 				<a href="#"><img src="<?php echo THEMEPATH; ?>images/social/em.png"></a>
 			</nav>
-			<div class="question"><div class="question-filler"></div><div class="mini-wrapper"><span>¿Lorem ipsum dolor sit amet, consectetur adipiscing<br>elit. Integer aliquam magna non erat semper?</span><br><a href="#">SI</a><a href="#">NO</a></div></div>
-			<div class="adver size3"></div>
-			<div class="comments"></div>
-			<div id="dissappear" class="mini-wrapper">
-				<a class="post mini" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur</span></a>
-				<a class="post mini" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur</span></a>
-				<a class="post mini" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur</span></a>
-				<a class="post mini" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur</span></a>
-			</div>
-			<div id="bottombar" style="margin-top:10px">
-				<a class="side-link" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur adipis</span></a>
-				<a class="side-link" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur adipis</span></a>
-				<a class="side-link" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur adipis</span></a>
-				<a class="side-link" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur adipis</span></a>
-			</div>
+				
 		</div>
 		<div class="sidebar"><div class="filler"></div>
-			<div class="adver size1" style="margin-top:15px"></div>
+			
 			<a class="ficha-link" href="#"><div></div><span>Lorem ipsum dolor sit amet co</span><span>123 FICHAS</span></a>
 			<a class="ficha-link" href="#"><div></div><span>Lorem ipsum dolor sit amet co</span><span>123 FICHAS</span></a>
 			<a class="ficha-link" href="#"><div></div><span>Lorem ipsum dolor sit amet co</span><span>123 FICHAS</span></a>
@@ -70,7 +53,7 @@
 			<a class="side-link" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur adipis</span></a>
 			<a class="side-link" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur adipis</span></a>
 			<a class="side-link" href="#"><div></div><span>Lorem ipsum dolor sit amet, consectetur adipis</span></a>
-			<div class="adver size1" style="margin-top:20px"></div>
+			
 		</div>
 	</div>
 </section>
