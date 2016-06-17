b625079cb33886c402a25fc93bf032c2dfcb6e84
Pablo Covarrubias <cova@covapro.local>
2016-06-17 18:40:37 -0500

    detalles

--- themes/plumasatomicas16/single-wadafact.php
+++ themes/plumasatomicas16/single-wadafact.php
@@ -45,7 +45,7 @@
 						<img src="<?php echo THEMEPATH; ?>images/postura.svg">
 						<div class="pointer" style="<?php echo 'left: '.($res_x-5).'%;'.'top: '.($res_y-5).'%;'; ?>"></div>
 					</div>
-					<span><?php echo $x_axis_name."-".$y_axis_name; ?></span>
+					<!-- <span><?php echo $x_axis_name."-".$y_axis_name; ?></span> -->
 			</div></a>
 			<div class="separador">
 				<span>FACT CHECKER</span>
