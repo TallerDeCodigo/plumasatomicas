46ae4e32efb2e47d20911ac71d5285f9cb7571aa
manuel <molop3@gmail.com>
2016-09-23 15:40:09 -0500

    merge w rouge1

---
+++ themes/plumasatomicas16/ce/fbvideos.php
@@ -0,0 +1,44 @@
+<?php 
+$url=substr(substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], '=')), 1);
+?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1, minimum-scale=1, height=device-height">
+		<meta http-equiv="cleartype" content="on">
+	</head>
+<body style="box-sizing:border-box;margin:0;">
+	<div id="fb-root"></div>
+	  <script>
+	  	   window.fbAsyncInit = function() {
+	  	     FB.init({
+	  	       appId      : '{1086508188112043}',
+	  	       xfbml      : true,
+	  	       version    : 'v2.5'
+	  	     });
+
+	  	    var my_video_player;
+	  	    FB.Event.subscribe('xfbml.ready', function(msg) {
+	  	        if (msg.type === 'video') {
+	  	           my_video_player = msg.instance;
+	  	        }
+	  	    });
+	  	   
+	  	   };
+
+	  	   (function(d, s, id){
+	  	      var js, fjs = d.getElementsByTagName(s)[0];
+	  	      if (d.getElementById(id)) {return;}
+	  	      js = d.createElement(s); js.id = id;
+	  	      js.src = "//connect.facebook.net/es_LA/sdk.js";
+	  	      fjs.parentNode.insertBefore(js, fjs);
+	  	    }(document, 'script', 'facebook-jssdk'));
+	  </script>
+	  <div class="fb-video"
+	    data-href="https://www.facebook.com/facebook/videos/<?php echo $url; ?>/"
+	    data-allowfullscreen="true"
+	    style="box-sizing:border-box;width:100%;"></div>
+</body>
+</html>
---
+++ themes/plumasatomicas16/page-fbvideos.php
@@ -0,0 +1,10 @@
+<?php
+ 	get_header();
+?>
+<section id="post-sec">
+	<div class="wrapper-special centrado">
+		dfgd
+	</div>
+</section>
+
+<?php get_footer(); ?>
